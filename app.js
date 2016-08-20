a48895304f703be8ce40d86c6ca4bd579ec0cde4
mbaquir <mohammed.baquir52@gmail.com>
2016-08-20 15:07:43 -0400

    Added all the stuff

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var request = require('request');
+var path = require('path');
+var router = express.Router();
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!');
+});
