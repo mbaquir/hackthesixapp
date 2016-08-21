7c5d7e65426345fcfae13c132730aaff18c97880
Jack L <Jack L>
2016-08-21 02:28:13 -0400

    integration

--- app.js
+++ app.js
@@ -5,6 +5,39 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var router = express.Router();
 var app = express();
+var Dictionary = require('./dictionary');
+var	request = require('request');
+var	express = require('express');
+var	bodyParser = require('body-parser');
+var	dict = new Dictionary({
+		key: "54447308-e899-4235-8c60-636df931ac75"
+});
+var translate = function(word, callback) {
+	var definition = '';
+	dict.define(word, function(error, result){
+		if (error == null) {
+			if(typeof result[0] != 'undefined'){
+				for(var i=0; i<1; i++){
+					definition += 'Part of speech: '+result[i].partOfSpeech+'\nDefinitions: '
+					+result[i].definition;
+				}
+			} else {
+				definition += 'Word not found';
+			}
+		}
+		else if (error === "suggestions"){
+			definition += word+' not found in dictionary. \nPossible suggestions: \n';
+			for (var i=0; i<3; i++){
+				definition += result[i]+'\n';
+			}
+		}
+		else {
+			console.log(error);
+			definition += error;
+		}
+		callback(definition);
+	});
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,10 +74,29 @@ app.get('/message', function(req, res) {
 app.post('/message', function(req, res) {
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
-    if (req.body.Body == 'Define') {
-        twiml.message('Define word!');
-    } else {
-        twiml.message('Invalid request. Please try a different command.');
+    console.log(req.query.Body);
+    var query = [];
+    try {
+    query = req.query.Body.split(' ').toLowerCase();
+	    if (query[0] !== undefined) {
+		    if (query[0] == 'define') {
+          var word = query[1];
+          if (word) {
+            translate(word, function(definition) {
+            	twiml.message(definition);
+            });
+          } else {
+
+          }
+		        // twiml.message('Define word!');
+
+		    } else {
+		        twiml.message('Invalid request. Please try a different command.');
+		    }
+	    }
+    } catch(err) {
+    	console.log(err);
+    	twiml.message('Invalid request. Please try a different command.' + err);
     }
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
