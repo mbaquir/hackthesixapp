8cf5204ace43e328957404d612fe858f7083452f
MATTHEW NGAI <MTFNGAI@uwaterloo.ca>
2016-08-21 06:22:13 -0400

    removing useless files

--- googleTranslate.js
+++
@@ -1,7 +0,0 @@
-var yandex = require('yandex-translate')('trnsl.1.1.20160821T044313Z.8fbdeed9f777bcbc.8bf304d8532c781d6e8f52dc8df4d3f833166087');
-var fs = require('fs');
-
-yandex.translate('Hello, my name is Jack', {to: 'zh'}, function(err, res) {
-  console.log(res.text);
-  fs.writeFile('output.txt', res.text);
-});
